1e9b82b545c33ced64751adf514e8644fd665359
ngquochuydl23 <nguyenquochuydl123@gmail.com>
2020-10-31 12:43:43 +0700

    init project

---
+++ n.work/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace n.work.Controllers
+{
+  public class OrderController : BaseApiController
+  {
+    
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+
+    }
+
+    [HttpPost("{id}")]
+    public void Post([FromBody] string value)
+    {
+
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+
+    }
+  }
+}
---
+++ n.work/Models/VerificationResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace n.work.Models
+{
+  public class VerificationResponse
+  {
+    public string AuthToken { get; set; }
+
+    public Account Account { get; set; }
+
+    public VerificationResponse(Account Account,string AuthToken)
+    {
+      this.Account = Account;
+      this.AuthToken = AuthToken;
+    }
+  }
+}
